767e63d02b2295332455cd2a0852eacd6bf406b5
emmaccen <oriolaemmanuel199@gmail.com>
2025-04-13 01:14:30 +0100

    Added peer connection username modal

---
+++ components/JoinConnectionUserNameModal.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useVisibilityState } from "@/app/store/modals";
+import { ModalIds } from "@/app/store/modals/types";
+import { usePeerState } from "@/app/store/peer";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+export const JoinConnectionUserNameModal = ({
+  children,
+}: {
+  children: React.JSX.Element;
+}) => {
+  const { updatePeerStatePartially, currentPeerState } = usePeerState();
+  const [username, setUserName] = useState(currentPeerState.username ?? "");
+  const { imVisible, hidePreviousThenShowNext, hideModal } =
+    useVisibilityState();
+
+  return (
+    <Dialog
+      open={imVisible(ModalIds.joinConnectionUserNameModal)}
+      onOpenChange={() => hideModal(ModalIds.joinConnectionUserNameModal)}
+    >
+      {children}
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Username</DialogTitle>
+          <DialogDescription className="text-left">
+            This is only used to identify your connection on other devices
+          </DialogDescription>
+        </DialogHeader>
+        <Input
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+          className="my-3"
+          id="username"
+          type="text"
+        />
+        <DialogFooter>
+          <Button
+            onClick={() => {
+              if (!username.trim()) return;
+              updatePeerStatePartially({
+                username: username,
+              });
+              hidePreviousThenShowNext(
+                ModalIds.joinConnectionUserNameModal,
+                ModalIds.qrScannerModal
+              );
+            }}
+            className="cursor-pointer"
+          >
+            Save Username
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
