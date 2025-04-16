289a05c9f12eb8073b484a56af82c4cea4b8c325
emmaccen <oriolaemmanuel199@gmail.com>
2025-04-16 16:14:49 +0100

    Added chat messages to chat panel/area

--- app/(mesh)/page.tsx
+++ app/(mesh)/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { hostState } from "@/app/store/host";
+import { HostStateType } from "@/app/store/host/types";
+import { useMessengerState } from "@/app/store/messenger";
+import { peerState } from "@/app/store/peer";
+import { PeerStateType } from "@/app/store/peer/types";
+import { ChatBubble } from "@/components/ui/chat/ChatBubble";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,7 +13,11 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { useConnectionStateManager } from "@/hooks/useConnectionStateManager";
+import { useSendMessage } from "@/hooks/useSendMessage";
 import { Paperclip, SendHorizontal } from "lucide-react";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 const UploadFileSelection = () => {
@@ -30,16 +40,76 @@ const UploadFileSelection = () => {
   );
 };
 export default function Page() {
+  const [message, setMessage] = useState("");
+  const { sendMessage } = useSendMessage();
+  const { values: host } = useConnectionStateManager<HostStateType>(hostState);
+  const { values: peer } = useConnectionStateManager<PeerStateType>(peerState);
+  const { currentMessengerState } = useMessengerState();
+  // const [isWho, setIsWho] = useState<"host" | "peer" | "unknown">("unknown");
+
+  const send = () => {
+    const id = nanoid(24);
+    if (host.peerConnection)
+      sendMessage(
+        {
+          id,
+          message,
+          timestamp: new Date().toISOString(),
+          sender: host.username ?? "host",
+          senderId: host.userId,
+          type: "message",
+        },
+        host.dataChannel
+      );
+    else
+      sendMessage(
+        {
+          id,
+          message,
+          timestamp: new Date().toISOString(),
+          sender: peer.username ?? "peer",
+          senderId: peer.userId,
+          type: "message",
+        },
+        peer.dataChannel
+      );
+    setMessage("");
+  };
+
+  // useEffect(() => {
+  //   if (host.peerConnection) setIsWho("host");
+  //   else if (peer.peerConnection) setIsWho("peer");
+  //   else setIsWho("unknown");
+  // }, [host.peerConnection, peer.peerConnection]);
+
   return (
     <>
       <ScrollArea className="flex-1 overflow-y-auto py-6 px-5 w-full max-w-[800px] mx-auto">
-        Content goes here
+        {currentMessengerState.messages.length > 0 && (
+          <div className="flex flex-col gap-2">
+            {currentMessengerState.messages.map((message) => {
+              return (
+                <div
+                  key={message.id}
+                  className={`flex ${
+                    message.senderId === (host.userId || peer.userId)
+                      ? "justify-end"
+                      : "justify-start"
+                  }`}
+                >
+                  <ChatBubble {...message} />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </ScrollArea>
       <div className="w-full max-w-[800px] mx-auto mb-6 px-5">
         <div className="rounded-b-lg w-full">
           <form
             onSubmit={(e) => {
               e.preventDefault();
+              send();
             }}
             className="relative flex w-full items-center gap-2"
           >
@@ -47,6 +117,12 @@ export default function Page() {
               <UploadFileSelection />
             </div>
             <TextareaAutosize
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              onKeyUp={(e) => {
+                if (e.key !== "Enter") return;
+                send();
+              }}
               autoComplete="off"
               name="message"
               placeholder="Send message..."
--- components/ui/chat/ChatBubble.tsx
+++ components/ui/chat/ChatBubble.tsx
@@ -7,7 +7,7 @@ export const ChatBubble = (message: Message) => {
       <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 rounded-e-xl rounded-es-xl dark:bg-card bg-gray-100">
         <div className="flex items-center space-x-2 rtl:space-x-reverse">
           <span className="text-sm font-semibold">{message.sender}</span>
-          <span className="text-sm font-normal">
+          <span className="text-sm text-gray-500 dark:text-gray-400">
             {new Date(message.timestamp).toLocaleTimeString([], {
               hour: "2-digit",
               minute: "2-digit",
