4596eadc33c871bcc125108bc00a2802967a719a
emmaccen <oriolaemmanuel199@gmail.com>
2025-04-21 22:32:01 +0100

    Updated messenger store

--- app/store/messenger/index.ts
+++ app/store/messenger/index.ts
@@ -13,17 +13,39 @@ export const useMessengerState = () => {
   const { updateStore: updateMessengerStatePartially } =
     useUpdateStore<MessengerStateType>(fileManagerState);
 
-  const updateMessage = (newMessage: Message) => {
+  const addNewMessage = (newMessage: Message) => {
     setCurrentMessengerState((prevState) => {
       return {
-        ...prevState,
         messages: [...prevState.messages, newMessage],
       };
     });
   };
+
+  const updateMessageById = (id: string, updatedMessage: Message) => {
+    setCurrentMessengerState((prevState) => {
+      const updatedMessages = prevState.messages.map((message) =>
+        message.id === id ? { ...message, ...updatedMessage } : message
+      );
+      return {
+        messages: updatedMessages,
+      };
+    });
+  };
+  const getMessageById = (id: string) => {
+    const message = currentMessengerState.messages.find(
+      (message) => message.id === id
+    );
+    if (!message) {
+      console.error("Message not found");
+      return null;
+    }
+    return message;
+  };
   return {
     currentMessengerState,
     updateMessengerStatePartially,
-    updateMessage,
+    addNewMessage,
+    updateMessageById,
+    getMessageById,
   };
 };
