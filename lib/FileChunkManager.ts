fbf6904ff9a45c8eae07532492789480b2109f6f
emmaccen <oriolaemmanuel199@gmail.com>
2025-04-23 23:19:29 +0100

    File-chunk-manager. A singleton to handle chunking

---
+++ lib/FileChunkManager.ts
@@ -0,0 +1,96 @@
+import { Message } from "@/app/store/messenger/types";
+
+class FileChunksManager {
+  private static instance: FileChunksManager;
+  private fileChunks: Map<
+    string,
+    {
+      chunks: {
+        [index: number]: number[];
+      };
+      message: Message;
+    }
+  >;
+
+  private constructor() {
+    this.fileChunks = new Map();
+  }
+
+  // Ensure only one instance exists
+  public static getInstance(): FileChunksManager {
+    if (!FileChunksManager.instance) {
+      FileChunksManager.instance = new FileChunksManager();
+    }
+    return FileChunksManager.instance;
+  }
+
+  // Add or update file chunk
+  public addChunk(fileId: string, message: Message) {
+    const fileData = this.fileChunks.get(fileId);
+
+    if (message.messageType === "metadata" && !fileData) {
+      console.log("Received metadata and adding to manager: ", message);
+      this.fileChunks.set(fileId, {
+        chunks: {},
+        message,
+      });
+      return;
+    }
+
+    if (message.messageType === "file" && fileData) {
+      if (
+        message.chunkData !== undefined &&
+        message.chunkIndex !== undefined &&
+        message.totalChunks !== undefined
+      )
+        fileData.chunks[message.chunkIndex] = message.chunkData;
+      fileData.message = message;
+      return;
+    }
+
+    if (message.messageType === "message") {
+      if (
+        !fileData &&
+        message.chunkData !== undefined &&
+        message.chunkIndex !== undefined &&
+        message.totalChunks !== undefined
+      ) {
+        this.fileChunks.set(fileId, {
+          chunks: {
+            [message.chunkIndex]: message.chunkData,
+          },
+          message,
+        });
+      } else if (
+        fileData &&
+        message.chunkData !== undefined &&
+        message.chunkIndex !== undefined &&
+        message.totalChunks !== undefined
+      ) {
+        fileData.chunks[message.chunkIndex] = message.chunkData;
+        // fileData.message = message;
+        return;
+      }
+    }
+  }
+
+  // Get file chunks by ID
+  public getFileChunk(fileId: string) {
+    console.log("Getting file chunk: ", this.fileChunks.get(fileId));
+    return this.fileChunks.get(fileId);
+  }
+
+  // Remove file entry
+  public removeFile(fileId: string) {
+    console.log("Removing file: ", fileId);
+    this.fileChunks.delete(fileId);
+  }
+
+  // Get entire Map
+  public getAllFiles() {
+    console.log("Getting all files: ", this.fileChunks);
+    return this.fileChunks;
+  }
+}
+
+export default FileChunksManager;
