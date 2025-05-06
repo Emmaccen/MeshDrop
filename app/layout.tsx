8c8d1d5142258fcd0cae45950308aca0c5691bce
emmaccen <oriolaemmanuel199@gmail.com>
2025-05-06 23:09:52 +0100

    SEO updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,50 @@
+import { metaData } from "@/app/_data/constants";
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: "MeshDrop",
-  description:
-    "A browser-based file transfer solution that enables seamless, direct device-to-device file sharing without the need for installation or cloud services",
+  title: metaData.title,
+  description: metaData.description,
+  applicationName: metaData.title,
+  referrer: "origin-when-cross-origin",
+  generator: "Next.js",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  category: "Technology",
+  twitter: {
+    card: "summary_large_image",
+    title: metaData.title,
+    description: metaData.description,
+  },
+  openGraph: {
+    title: metaData.title,
+    description: metaData.description,
+    siteName: metaData.title,
+    locale: "en_US",
+    type: "website",
+  },
+  appleWebApp: {
+    title: metaData.title,
+    statusBarStyle: "black-translucent",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
